2e5050d2a75fbc60aae837e05cd9ce272056fa09
YairZen <zeneve.yair@gmail.com>
2025-10-26 10:54:57 +0200

    finish

--- zcross_simple_example.py
+++ zcross_simple_example.py
@@ -1,14 +1,12 @@
+
 #imports
 import numpy as np
 
 def zero_cross(ar_of_a_sampled_f):
-
-    signs = np.sign(ar_of_a_sampled_f)
-
-
-    sign_changes = np.diff(signs)
-    ar_of_sign_change_indxs = np.nonzero(sign_changes)[0]
-    return ar_of_sign_change_indxs
+    #your code
+    H1t = np.sign(ar_of_a_sampled_f)
+    H1s = np.abs(H1t[:-1] - H1t[1:])
+    return np.nonzero(H1s)[0]
 
 # input
 v0=float(input())
@@ -16,11 +14,11 @@ v0=float(input())
 g=9.8 # m/s^2
 t=np.arange(0,5,0.01)
 y=-0.3+v0*t-0.5*g*t**2
+
 ## your code
 
-t0, t1 = zero_cross(y)
-
-time_of_flight= np.abs(t1-t0)
+z_idx = zero_cross(y)
+time_of_flight= t[z_idx[1]] - t[z_idx[0]]
 
 #output
 print(f'time of flight: {time_of_flight:.5g} s')
